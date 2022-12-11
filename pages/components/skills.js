adaef95a0b9d2dfb73a21098e2d5d4df7b1e255f
Kevin Herrera <kevinhl_2998@hotmail.com>
2022-12-11 17:33:06 -0600

    cards color

--- pages/components/skills.js
+++ pages/components/skills.js
@@ -12,10 +12,10 @@ export default function Skills() {
 
       <div className=" flex flex-row justify-center items-center w-full mt-20">
         <div className=" grid grid-cols-2 w-full gap-10 px-[500px]">
-          <div className=" bg-[#2a3163] w-full h-[500px] rounded-md">
+          <div className=" bg-[#1f3a8a] w-full h-[500px] rounded-md">
             <div className=" flex flex-col w-full justify-center items-center"></div>
           </div>
-          <div className=" bg-[#2a3163] w-full h-[500px] rounded-md">
+          <div className=" bg-[#1f3a8a] w-full h-[500px] rounded-md">
             <div className=" flex flex-col w-full justify-center items-center"></div>
           </div>
         </div>
