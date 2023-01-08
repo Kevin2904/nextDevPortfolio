b822c10fb102d8721fe0ace51d4d0da403b9c908
Kevin Herrera <kevinhl_2998@hotmail.com>
2023-01-07 23:41:46 -0600

    fix: load time

--- pages/index.js
+++ pages/index.js
@@ -12,7 +12,7 @@ import Navbar from "./shared/navbar";
 import Splash from "./shared/splash";
 
 export default function Home() {
-  const [loading, setLoading] = useState(false);
+  const [loading, setLoading] = useState(true);
   const [windowState, setWindowState] = useState(undefined);
   useEffect(() => {
     console.log(loading);
